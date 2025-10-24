3c10890478d39c5ebe38efd7204817008f3af8ba
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2025-10-23 22:33:14 -0300

    feat: robustecer ruta de archivo de datos

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from pathlib import Path
 
-def load_raw_data(ruta="./data/raw/credit_defaults.csv"):
+def load_raw_data():
     """
     Carga datos crudos desde ./data/raw
     """
+    ruta = Path(__file__).resolve().parents[2] / "data" / "raw" / "credit_defaults.csv"
     dataset = pd.read_csv(ruta, sep=";", header=1)
     return dataset
