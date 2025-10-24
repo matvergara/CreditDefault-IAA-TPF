bf4f8442a6be79eae49b2fd2ac3f1adce42e5293
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2025-10-23 22:10:06 -0300

    feat: modificar funciones de carga de datos

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def load_raw_data():
+def load_raw_data(ruta="./data/raw/credit_defaults.csv"):
     """
     Carga datos crudos desde ./data/raw
     """
-    dataset = pd.read_csv("./data/raw/credit_defaults.csv", sep=";", header=1)
+    dataset = pd.read_csv(ruta, sep=";", header=1)
     return dataset
