6dcbc3fe431feca410af7f4db46d083e2d2e047a
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2025-12-03 17:03:59 -0300

    refactor: cambiar ruta en load_data.py

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,10 +1,8 @@
 import pandas as pd
-from pathlib import Path
 
 def load_raw_data():
     """
     Carga datos crudos desde ./data/raw
     """
-    ruta = Path(__file__).resolve().parents[2] / "data" / "raw" / "credit_defaults.csv"
-    dataset = pd.read_csv(ruta, sep=";", header=1)
+    dataset = pd.read_csv('../data/raw/credit_defaults.csv', sep=";", header=1)
     return dataset
