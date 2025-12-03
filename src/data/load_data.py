15b31c515fbc63eaa1c6ba46ed7cf88d7a7a8626
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2025-12-03 19:01:51 -0300

    feat: agregar save_processed_data() para guardar datos procesados

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,7 +2,17 @@ import pandas as pd
 
 def load_raw_data():
     """
-    Carga datos crudos desde ./data/raw
+    Carga datos crudos desde data/raw
     """
     dataset = pd.read_csv('../data/raw/credit_defaults.csv', sep=";", header=1)
     return dataset
+
+def save_processed_data(df: pd.DataFrame, path):
+    """
+    Guarda archivo preprocesado en data/processed
+    
+    Args:
+        df: Dataframe preprocesado
+    Returns
+    """
+    df.to_csv(path, index=False)
