970dc12b7e2ed0c72c93c2deb6cffc79caf24e40
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2025-12-08 12:53:13 -0300

    docs: modificar Docstrings y Type Hinting

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
-def load_raw_data():
+def load_raw_data() -> pd.DataFrame:
     """
     Carga datos crudos desde data/raw
+
+    Returns:
+        dataset (pd.DataFrame): Dataframe con datos crudos 
     """
     dataset = pd.read_csv('../data/raw/credit_defaults.csv', sep=";", header=1)
     return dataset
 
-def save_processed_data(df: pd.DataFrame, path = '../data/processed/credit_defaults_clean.csv'):
+def save_processed_data(df: pd.DataFrame, path = '../data/processed/credit_defaults_clean.csv') -> None:
     """
     Guarda archivo preprocesado en data/processed
     
     Args:
-        df: Dataframe preprocesado
-    Returns
-        None. Guarda el csv en el path indicado
+        df (pd.DataFrame): Dataframe preprocesado
+        path (str): Ruta para guardar el csv preprocesado
+    
+    Returns:
+        None: Guarda el csv en el path indicado
     """
     df.to_csv(path, index=False)
